4a4fc96e9ad119761629363033a1eec1191d26aa
LLKY <lesterlohky@gmail.com>
2024-02-05 16:58:41 +0800

    Server/Client ready

---
+++ client/client.py
@@ -0,0 +1,118 @@
+import csv, time, requests, hashlib
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
+# ==================================
+# Load API endpoints
+# ==================================
+
+endpoint = config["ENDPOINT"]
+endpointTest = config["ENDPOINT_TEST"]
+# 'https://api.binance.com/api/v3/order'
+
+# ==================================
+# Load Orders and Precision files
+# ==================================
+
+# Load Precision.csv into precision dictionary
+precisionFile = open('Precision.csv', newline='')
+precisionFile = csv.reader(precisionFile, delimiter=',')
+
+users = {}
+
+next(precisionFile, None) # Skip header
+for row in precisionFile:
+    users[row[0]] = {"pricePrecision": row[1], "quantityPrecision": row[2]}
+
+# Load Orders.csv into orders list
+ordersFile = open('Orders.csv', newline='')
+ordersFile = csv.reader(ordersFile, delimiter=',')
+
+orders = []
+# Pair / Direction / Price / Quantity / Account / Value
+# JTOUSDT / BUY / 1.7101 / 8.000 / 3 / 13.68
+next(ordersFile, None) # Skip header
+for row in ordersFile:
+    orders.append({"pair": row[0], "direction": row[1], "price": row[2], "quantity": row[3], "account": row[4]})
+
+keystroke = input(f"{len(users)} accounts and {len(orders)} orders loaded. To proceed, input Y. To do a test run, input T. To abort, input N: ")
+
+while True:
+    if keystroke.casefold() == "n":
+        exit()
+    elif keystroke.casefold() == "y":
+        print("Proceeding with order placement")
+        break
+    elif keystroke.casefold() == "t":
+        print("Running test order placement")
+        break
+    else:
+        keystroke = input("Please input Y, T, or N: ")
+
+# ==================================
+# Load private keys, API keys, and API endpoint
+# ==================================
+
+# For simulation purposes, the private key is stored unencrypted in the .env file
+users["1"]["privateKey"] = config["PRIVATE_KEY1"]
+users["1"]["apiKey"] = config["API_KEY1"]
+
+users["2"]["privateKey"] = config["PRIVATE_KEY2"]
+users["2"]["apiKey"] = config["API_KEY2"]
+
+users["3"]["privateKey"] = config["PRIVATE_KEY3"]
+users["3"]["apiKey"] = config["API_KEY3"]
+
+
+# ==================================
+# Order execution
+# ==================================
+        
+for order in orders:
+    # Round price and quantity to specified decimal places
+    pricePrecision = int(users[order["account"]]["pricePrecision"])
+    quantityPrecision = int(users[order["account"]]["quantityPrecision"])
+    order["price"] = round(float(order["price"]), pricePrecision)
+    order["quantity"] = round(float(order["quantity"]), quantityPrecision)
+
+    # Add request headers
+    headers = {
+	    "X-MBX-APIKEY": users[order["account"]]["apiKey"]
+    }
+
+    # Add request params
+    params = {
+        "symbol": order["pair"],
+	    "side": order["direction"],
+	    "type": "LIMIT",
+	    "quantity": str(order["quantity"]),
+	    "price": str(order["price"]),
+	    "timeInForce": "GTC"
+    }
+
+    # Add timestamp
+    timestamp = int(time.time() * 1000) # UNIX timestamp in milliseconds
+    params['timestamp'] = timestamp
+
+    # Sign request using a simulated encryption method
+    hashObject = hashlib.sha256()
+    hashObject.update(users[order["account"]]["privateKey"].encode())
+    hashPassword = hashObject.hexdigest()
+    params['signature'] = hashPassword
+
+    if keystroke.casefold() == "y":
+        response = requests.post(endpoint, headers=headers, data=params)
+    elif keystroke.casefold() == "t":
+        response = requests.post(endpointTest, headers=headers, data=params)
+    if (response.ok):
+        json = response.json()
+        print(f'[{json['message']}] AccountID: {order['account']}, Balance: {json['balance']}')
+    else:
+        print(f"[Failed: {response.text}] AccountID: {order['account']}, {order["pair"]} {order["quantity"]} @ ${order["price"]}")
+    time.sleep(0.1) # Sleep 100ms to adhere to API rate limit
+
+if keystroke.casefold() == "y":
+    print("Order placement complete")
+elif keystroke.casefold() == "t":
+    print("Test order placement complete")
---
+++ exchange/exchange.py
@@ -0,0 +1,182 @@
+from flask import Flask, request, jsonify
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from dotenv import dotenv_values
+import mysql.connector, hashlib
+
+config = dotenv_values(".env")
+
+app = Flask(__name__)
+
+mydb = mysql.connector.connect(
+    host=config["SQLHOST"],
+    user=config["SQLUSER"],
+    password=config["SQLPASSWORD"],
+    port=config["SQLPORT"]
+)
+
+mycursor = mydb.cursor()
+
+limiter = Limiter(
+    app=app,
+    key_func=get_remote_address,
+    default_limits=["300000 per day", "36000 per hour"]
+)
+
+# ==================================
+# MySQL Table initialization
+# ==================================
+def createTables():
+    mycursor.execute("SHOW TABLES LIKE 'users'")
+    if not mycursor.fetchone():
+        print("User table does not exist, creating...")
+        mycursor.execute("CREATE TABLE users (account_id BIGINT NOT NULL, balance DOUBLE, private_key VARCHAR(255), PRIMARY KEY (account_id))")
+        # account_id DOUBLE
+        # balance BIGINT
+        # private_key VARCHAR(255)
+
+        sql = "INSERT INTO users (account_id, balance, private_key) VALUES (%s, %s, %s)"
+        val = [
+        (1, 1000, 'abc123'),
+        (2, 20000, 'ijk123'),
+        (3, 123456789, 'xyz123')
+        ]
+
+        mycursor.executemany(sql, val)
+        mydb.commit()
+
+    mycursor.execute("SHOW TABLES LIKE 'apikeys'")
+    if not mycursor.fetchone():
+        print("API Keys table does not exist, creating...")
+        mycursor.execute("CREATE TABLE apikeys (api_key VARCHAR(255), account_id BIGINT, FOREIGN KEY (account_id) REFERENCES users(account_id))")
+        # api_key VARCHAR(255)
+        # account_id BIGINT
+
+        sql = "INSERT INTO apikeys (api_key, account_id) VALUES (%s, %s)"
+        val = [
+        ('123', 1),
+        ('456', 2),
+        ('789', 3),
+        ]
+        
+        mycursor.executemany(sql, val)
+        mydb.commit()
+
+    mycursor.execute("SHOW TABLES LIKE 'orders'")
+    if not mycursor.fetchone():
+        print("Order table does not exist, creating...")
+        mycursor.execute("CREATE TABLE orders (symbol VARCHAR(255), side VARCHAR(255), type VARCHAR(255), quantity DOUBLE, price DOUBLE, time_in_force VARCHAR(255), account_id BIGINT, FOREIGN KEY (account_id) REFERENCES users(account_id))")
+        # symbol VARCHAR(255)
+        # side VARCHAR(255)
+        # type VARCHAR(255)
+        # quantity DOUBLE
+        # price DOUBLE
+        # time_in_force VARCHAR(255)
+        # account_id BIGINT
+
+try:
+    mycursor.execute("USE exchange")
+except:
+    print("Database does not exist, creating...")
+    mycursor.execute("CREATE DATABASE exchange")
+    mycursor.execute("USE exchange")
+print("Connected to exchange database")
+createTables()
+
+# ==================================
+# Server Routes
+# ==================================
+
+# Root. Test to see if the server is running.
+@app.route("/")
+@limiter.limit("10 per second")
+def welcome():
+    return "Welcome to the Cinance Exchange!"
+
+# Order route, rate limited to 1 request per 100 milliseconds.
+@app.route("/api/order", methods = ['POST'])
+@limiter.limit("10 per second")
+def order():
+    try:
+        # Fetch account info based on API key
+        apiKey = request.headers.get("X-MBX-APIKEY")
+        mycursor.execute('SELECT account_id FROM apikeys WHERE api_key = %s', (apiKey,))
+        accountId = mycursor.fetchone()[0]
+        mycursor.execute('SELECT * FROM users WHERE account_id = %s', (accountId,))
+        accountInfo = mycursor.fetchone()
+
+        # Verify private key
+        data = request.form.to_dict()
+        hashObject = hashlib.sha256()
+        privateKey = accountInfo[2]
+        hashObject.update(privateKey.encode())
+        hashPassword = hashObject.hexdigest()
+        if not data['signature'] == hashPassword:
+            return "Bad request", 400
+        
+        # Check balance
+        balance = accountInfo[1]
+        price = float(data["price"])
+        quantity = float(data["quantity"])
+        value = round(price * quantity,10)
+        print(f"Balance: {balance}, Price: {price}, Quantity: {quantity}, Total: {value}")
+        if (balance < value):
+            return "Insufficient balance", 400
+        
+        # Deduct balance from account
+        newBalance = round(balance - value,10)
+        mycursor.execute('UPDATE users SET balance = %s WHERE account_id = %s', (newBalance, accountId))
+
+        # Generate new order
+        mycursor.execute('INSERT INTO orders VALUES (%s, %s, %s, %s, %s, %s, %s)', (data["symbol"], data["side"], data["type"], data["quantity"], data["price"], data["timeInForce"], accountId))
+        mydb.commit()
+
+        response = jsonify(message='Order placed', balance=round(newBalance,2))
+        return response
+    except:
+        return "Bad request", 400
+
+# Test order route, rate limited to 1 request per 100 milliseconds. Does not save anything to DB.
+@app.route("/api/order_test", methods = ['POST'])
+@limiter.limit("10 per second")
+def order_test():
+    try:
+        # Fetch account info based on API key
+        apiKey = request.headers.get("X-MBX-APIKEY")
+        mycursor.execute('SELECT account_id FROM apikeys WHERE api_key = %s', (apiKey,))
+        accountId = mycursor.fetchone()[0]
+        mycursor.execute('SELECT * FROM users WHERE account_id = %s', (accountId,))
+        accountInfo = mycursor.fetchone()
+
+        # Verify private key
+        data = request.form.to_dict()
+        hashObject = hashlib.sha256()
+        privateKey = accountInfo[2]
+        hashObject.update(privateKey.encode())
+        hashPassword = hashObject.hexdigest()
+        if not data['signature'] == hashPassword:
+            return "Bad request", 400
+        
+        # Check balance
+        balance = accountInfo[1]
+        price = float(data["price"])
+        quantity = float(data["quantity"])
+        value = round(price * quantity,10)
+        print(f"Balance: {balance}, Price: {price}, Quantity: {quantity}, Total: {value}")
+        if (balance < value):
+            return "Insufficient balance", 400
+
+        response = jsonify(message='Test order placed', balance=round(balance - value,2))
+        return response
+    except:
+        return "Bad request", 400
+    
+# Reset all DBs to their starting state.
+@app.route("/api/reset")
+def reset():
+    mycursor.execute("DROP TABLE IF EXISTS apikeys")
+    mycursor.execute("DROP TABLE IF EXISTS orders")
+    mycursor.execute("DROP TABLE IF EXISTS users")
+    createTables()
+    print("Tables reset")
+    return "Tables reset"
